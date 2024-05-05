bf60312a7273b67c26688d5e3259e373df58a07d
Allen Hein <lerover1155@gmail.com>
2024-05-05 09:39:54 +0630

    first add

---
+++ firstPractice/viscard/script.js
@@ -0,0 +1,13 @@
+// const getcard = document.getElementById('card')
+// const getcardnumber = document.getElementById('card-number');
+// const getcardnospan = document.getElementById('border');
+
+// getcardnumber.addEventListener('focus',function(){
+//     getcardnospan.style='transform:translateX(0) translateY(5px);'
+    
+// })
+
+// getcard.addEventListener('click',function(){
+//     getcardnospan.style = ''
+// })
+
